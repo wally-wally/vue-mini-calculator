fdd9bbf5055da749a3cfd9d26189cafb92b1e963
wally-wally <wallys0213@gmail.com>
2021-05-16 18:06:18 +0900

    update: 계산 로직 작업 중간 반영

--- src/components/Calculator/Calculator.vue
+++ src/components/Calculator/Calculator.vue
@@ -1,19 +1,85 @@
 <template>
 	<section class="calculator__wrapper">
-		<ShowResult />
-		<Buttons />
+		<Result :numbers="numbers" :sign="sign" :tempValue="tempValue" :mode="mode" />
+		<Buttons @clickNumber="setNumber" @clickSign="setSign" @clickResult="showResult" @clickReset="reset" />
 	</section>
 </template>
 
 <script>
-import ShowResult from '@/components/Calculator/ShowResult.vue';
+import Result from '@/components/Calculator/Result.vue';
 import Buttons from '@/components/Calculator/Buttons.vue';
+import common from '@/common.js';
+
+const calculatorMode = common.mode;
 
 export default {
 	name: 'Calculator',
 	components: {
-		ShowResult,
+		Result,
 		Buttons,
+	},
+
+	data() {
+		return {
+			numbers: [],
+			sign: '+',
+			tempValue: '',
+			mode: calculatorMode.SHOW_RESULT_MODE,
+		};
+	},
+
+	methods: {
+		setNumber(number) {
+			if (this.isZeroCase(number)) {
+				return;
+			}
+
+			this.changeTypeNumberMode();
+
+			this.tempValue += number;
+		},
+
+		setSign(sign) {
+			this.changeShowResultMode();
+
+			this.showResult();
+
+			this.sign = sign;
+			this.tempValue = '';
+		},
+
+		showResult() {
+			if (this.isEmptyTempValue()) {
+				return;
+			}
+
+			this.changeShowResultMode();
+
+			this.numbers.push(`${this.sign}${this.tempValue}`);
+		},
+
+		reset() {
+			this.changeShowResultMode();
+
+			this.numbers = [];
+			this.tempValue = '';
+		},
+
+		isZeroCase(value) {
+			return value === '0' && this.isEmptyTempValue();
+		},
+
+		isEmptyTempValue() {
+			return this.tempValue === '';
+		},
+
+		changeTypeNumberMode() {
+			this.mode = calculatorMode.TYPE_NUMBER_MODE;
+		},
+
+		changeShowResultMode() {
+			this.mode = calculatorMode.SHOW_RESULT_MODE;
+		},
 	},
 };
 </script>
