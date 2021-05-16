13a4206062169497d51e870d347eed012c035432
wally-wally <wallys0213@gmail.com>
2021-05-16 18:05:18 +0900

    update: 계산기 버튼 관련 emit event 구성

--- src/components/Calculator/Buttons.vue
+++ src/components/Calculator/Buttons.vue
@@ -1,12 +1,22 @@
 <template>
 	<article class="buttons">
 		<div class="buttons__number">
-			<button class="buttons__number--item" v-for="(number, index) in numbers" :key="`number-button-${index}`">
+			<button
+				class="buttons__number--item"
+				v-for="(number, index) in numbers"
+				:key="`number-button-${index}`"
+				@click="clickNumber(number)"
+			>
 				{{ number }}
 			</button>
 		</div>
 		<div class="buttons__symbol">
-			<button class="buttons__symbol--item" v-for="(symbol, index) in symbols" :key="`symbol-button-${index}`">
+			<button
+				class="buttons__symbol--item"
+				v-for="(symbol, index) in symbols"
+				:key="`symbol-button-${index}`"
+				@click="clickSymbol(symbol)"
+			>
 				{{ symbol }}
 			</button>
 		</div>
@@ -14,13 +24,41 @@
 </template>
 
 <script>
+const numbers = Array(10)
+	.fill()
+	.map((_, index) => String(index));
+
 export default {
 	name: 'Buttons',
 	data() {
 		return {
-			numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+			numbers,
 			symbols: ['+', '-', '=', 'Reset'],
 		};
+	},
+
+	methods: {
+		clickNumber(number) {
+			this.$emit('clickNumber', number);
+		},
+
+		clickSymbol(symbol) {
+			this.$emit(this.setEmitEventName(symbol), symbol);
+		},
+
+		setEmitEventName(symbol) {
+			if (symbol === '+' || symbol === '-') {
+				return 'clickSign';
+			}
+
+			if (symbol === '=') {
+				return 'clickResult';
+			}
+
+			if (symbol === 'Reset') {
+				return 'clickReset';
+			}
+		},
 	},
 };
 </script>
